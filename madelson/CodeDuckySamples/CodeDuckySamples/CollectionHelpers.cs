cc4bc203c956f7ae604b5813afd44f3951701da1
Michael Adelson <mike.adelson314@gmail.com>
2014-02-23 17:15:46 -0500

    work for 10 utils part 2

--- madelson/CodeDuckySamples/CodeDuckySamples/CollectionHelpers.cs
+++ madelson/CodeDuckySamples/CodeDuckySamples/CollectionHelpers.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeDucky
+﻿namespace CodeDucky
 {
-    class CollectionHelpers
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class CollectionHelpers
     {
+        public static bool CollectionEquals<T>(this IEnumerable<T> @this, IEnumerable<T> that, IEqualityComparer<T> comparer = null)
+        {
+            // this is optional; if you want to be consistent with SequenceEqual, just throw exceptions if either argument is null instead
+            if (@this == null) { return that == null; }
+            else if (that == null) { return false; }
+
+            var countedItems = @this.GroupBy(t => t, comparer).ToDictionary(
+                g => g.Key,
+                g => g.Count(),
+                comparer);
+            foreach (var item in that)
+            {
+                int count;
+                if (!countedItems.TryGetValue(item, out count)) { return false; }
+                if (count - 1 == 0) { countedItems.Remove(item); }
+                else { countedItems[item] = count - 1; }
+            }
+            return countedItems.Count == 0;
+        }
+
+        /// <summary>
+        /// If the given key is present in the dictionary, returns the value for that key. Otherwise, executes the given value factory
+        /// function, adds the result to the dictionary, and returns that result
+        /// </summary>
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, Func<TKey, TValue> valueFactory)
+        {
+            TValue value;
+            if (!@this.TryGetValue(key, out value)) { @this.Add(key, value = valueFactory(key)); }
+            return value;
+        }
     }
 }
--- madelson/CodeDuckySamples/CodeDuckySamples/ReflectionHelpers.cs
+++ madelson/CodeDuckySamples/CodeDuckySamples/ReflectionHelpers.cs
@@ -3,10 +3,42 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
     using System.Text;
     using System.Threading.Tasks;
 
     public static class ReflectionHelpers
     {
+        /// <summary>
+        /// Gets generic arguments from the given type for the given type definition
+        /// </summary>
+        public static Type[] GetGenericArguments(this Type @this, Type genericTypeDefinition)
+        {
+            Throw.IfNull(@this, "this");
+            Throw.IfNull(genericTypeDefinition, "genericTypeDefinition");
+            Throw.If(!genericTypeDefinition.IsGenericTypeDefinition, "genericTypeDefinition: must be a generic type definition");
+
+            if (genericTypeDefinition.IsInterface)
+            {
+                var @interface = @this.GetInterfaces().FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericTypeDefinition);
+                return @interface.NullSafe(i => i.GetGenericArguments(), Type.EmptyTypes);
+            }
+            if (@this.IsGenericType && @this.GetGenericTypeDefinition() == genericTypeDefinition)
+            {
+                return @this.GetGenericArguments();
+            }
+            return @this.BaseType.NullSafe(t => t.GetGenericArguments(genericTypeDefinition), Type.EmptyTypes);
+        }
+
+        /// <summary>
+        /// Returns the method referenced by the given expression
+        /// </summary>
+        public static MethodInfo GetMethod<TInstance>(Expression<Action<TInstance>> methodExpression)
+        {
+            Throw.IfNull(methodExpression, "methodExpression");
+            var methodCall = (MethodCallExpression)methodExpression.Body;
+            return methodCall.Method;
+        }
     }
 }
